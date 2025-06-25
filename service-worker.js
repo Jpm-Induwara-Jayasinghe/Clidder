404fb4e512e74ba03b3baf2439f8488f65366d48
Jpm-Induwara-Jayasinghe <jm.induwarapjayasinghe@gmail.com>
2025-06-25 21:17:46 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+// service-worker.js
+self.addEventListener("install", event => {
+  console.log("Service worker installed.");
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
+});
